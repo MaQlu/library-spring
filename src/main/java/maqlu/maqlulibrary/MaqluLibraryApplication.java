0f8f25c633b1b2f0645fd9176e9cf50c0a79fa83
MaQlu <bartlomiejbernat@MacBook-Pro-Bartomiej.local>
2024-01-08 17:15:05 +0100

    start here

---
+++ src/main/java/maqlu/maqlulibrary/MaqluLibraryApplication.java
@@ -0,0 +1,13 @@
+package maqlu.maqlulibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaqluLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaqluLibraryApplication.class, args);
+	}
+
+}
