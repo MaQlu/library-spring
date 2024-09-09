e7ea20d81ddeefa5df278cd294d9c0073b1ee4b3
maqlu <b.bartek450@interia.pl>
2024-09-09 09:13:36 +0200

    deleted "json" trash file

--- src/main/java/maqlu/maqlulibrary/MaqluLibraryApplication.java
+++ src/main/java/maqlu/maqlulibrary/MaqluLibraryApplication.java
@@ -1,7 +1,6 @@
 package maqlu.maqlulibrary;
 
 import maqlu.maqlulibrary.utilities.FillDB;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,18 +8,20 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MaqluLibraryApplication {
+	public MaqluLibraryApplication(FillDB fillDB) {
+		this.fillDB = fillDB;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MaqluLibraryApplication.class, args);
 	}
 
-	@Autowired
+	final
 	FillDB fillDB;
 
 	@Bean
 	CommandLineRunner runner(){
-		return args -> {
-			fillDB.FillDB();
-		};
+		return args -> fillDB.FillDB();
 	}
 
 }
